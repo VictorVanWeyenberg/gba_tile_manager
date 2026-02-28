aef3871ab07ce9b7f25736545295a7f98f3ad8eb
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2026-02-28 18:52:31 +0100

    Add tile.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod color;
 mod palette;
+mod tile;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/tile.rs
@@ -0,0 +1,70 @@
+use std::ops::{Deref, DerefMut, Shl, Shr};
+
+struct Tile {
+    palette_indexes: [u8; 64] // Linear
+}
+
+impl Tile {
+    pub fn new(palette_indexes: [u8; 64]) -> Self {
+        Self { palette_indexes }
+    }
+}
+
+impl Deref for Tile {
+    type Target = [u8; 64];
+
+    fn deref(&self) -> &Self::Target {
+        &self.palette_indexes
+    }
+}
+
+impl DerefMut for Tile {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.palette_indexes
+    }
+}
+
+impl Into<Vec<u8>> for Tile {
+    fn into(self) -> Vec<u8> {
+        self.palette_indexes.chunks_exact(2)
+            .map(|chunk| (chunk[0] & 0xf) | (chunk[1] & 0xf).shl(4))
+            .collect()
+    }
+}
+
+impl From<Vec<u8>> for Tile {
+    fn from(value: Vec<u8>) -> Self {
+        let value = &value[0..32];
+        let palette_indexes = value.into_iter()
+            .map(|ns| [ns & 0xf, (ns & 0xf0).shr(4)])
+            .flatten()
+            .collect::<Vec<u8>>()
+            .try_into()
+            .unwrap();
+        Self { palette_indexes }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::tile::Tile;
+
+    #[test]
+    fn tile_round_trip() {
+        let mut palette_indexes: [u8; 64] = [ 0u8; 64 ];
+        for it in 1..=8 {
+            palette_indexes[it] = it as u8;
+        }
+
+        let tile = Tile::new(palette_indexes);
+        let bytes: Vec<u8> = tile.into();
+
+        assert_eq!(&bytes[..5], &[0x10, 0x32, 0x54, 0x76, 0x08]);
+        assert_eq!(&bytes[5..], &[0u8; 27]);
+
+        let tile = Tile::from(bytes);
+
+        assert_eq!(&tile[..9], &[0, 1, 2, 3, 4, 5, 6, 7, 8]);
+        assert_eq!(&tile[9..], &[0u8; 55]);
+    }
+}
